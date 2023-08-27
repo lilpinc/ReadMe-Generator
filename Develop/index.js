0bc8bff4bca0aae9656bc624d67beae94611ec71
lilpinc <133043171+lilpinc@users.noreply.github.com>
2023-08-27 13:12:41 -0400

    move outline of readme over to generateMarkdown.js, add function to call questions array to node, created the function init (). Still need fix license functions

--- Develop/index.js
+++ Develop/index.js
@@ -1,106 +1,79 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
+const generateMarkdown = require("./utils/generateMarkdown");
+
 console.log("Welcome to my ReadMe Generator");
 console.log("Please answer all questions to generate your quality Readme!")
 
-const generateReadMe = ({ title, description, installation, usage, license, contributing, tests }) =>
-`# ${title}
-# Description
-${description}
-
-*[Installation](#installation)
-*[Usage](#usage)
-*[Contribution](#contribution)
-*[Credits](#credits)
-*[License](#license)
-
-# Installation
-${installation}
-## Usage
-${usage}
-## Contribution
-${contributing}
-## Test Instructions
-${tests}
-## License
-${license}
-
-# Contact
-`;
-    // TODO: Create an array of questions for user input
-    inquirer
-        .prompt([
-            {
-                type: 'input',
-                name: 'title',
-                message: 'What is your project title?',
-                default: 'Project Title',
-                validate: (answer) => (answer.length > 1) ? true : console.log('Please enter a response for your project title')
-            },
-            {
-                type: 'input',
-                name: 'description',
-                message: 'What is your project description?',
-                default: 'Project Description',
-                validate: (answer) => (answer.length > 1) ? true : console.log('Please enter a response for your project description')
-            },
-            // table of contents
-            {
-                type: 'input',
-                name: 'installation',
-                message: 'What is your project installation instructions?',
-                default: 'Project Installation Instructions',
-                validate: (answer) => (answer.length > 1) ? true : console.log('Please enter a response for your project installation instructions')
-            },
-            {
-                type: 'input',
-                name: 'usage',
-                message: 'What is your project usage information?',
-                default: 'Project Usage',
-                validate: (answer) => (answer.length > 1) ? true : console.log('Please enter a response for your project usage')
-            },
-            {
-                type: 'list',
-                name: 'license',
-                message: 'What license would you like to use for this project?',
-                choices: ['Apache License 2.0', 'GNU General Public License v3.0', 'MIT License', 'BSD 2-Clause "Simplified" License', 'BSD 3-Clause "New" or "Revised" License', 'Boost Software License 1.0', 'Creative Commons Zero v1.0 Universal', 'Eclipse Public License 2.0', 'GNU Affero General Public License v3.0', 'GNU General Public License v2.0', 'GNU Lesser General Public License v2.1', 'Mozilla Public License 2.0', 'The Unlicense'],
-                // default: 'MIT License',
-            },
-            {
-                type: 'input',
-                name: 'contributing',
-                message: 'What are your project contribution guidelines?',
-                default: 'Project Contributions',
-                validate: (answer) => (answer.length > 1) ? true : console.log('Please enter a response for your project contributions')
-            },
-            {
-                type: 'input',
-                name: 'tests',
-                message: 'What are your project test instructions?',
-                default: 'Project Test Instructions',
-                validate: (answer) => (answer.length > 1) ? true : console.log('Please enter a response for your project test instructions')
-            }
-        ])
+// TODO: Create an array of questions for user input
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is your project title?',
+        default: 'Project Title',
+        validate: (answer) => (answer.length > 1) ? true : console.log('Please enter a response for your project title')
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'What is your project description?',
+        default: 'Project Description',
+        validate: (answer) => (answer.length > 1) ? true : console.log('Please enter a response for your project description')
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'What is your project installation instructions?',
+        default: 'Project Installation Instructions',
+        validate: (answer) => (answer.length > 1) ? true : console.log('Please enter a response for your project installation instructions')
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'What is your project usage information?',
+        default: 'Project Usage',
+        validate: (answer) => (answer.length > 1) ? true : console.log('Please enter a response for your project usage')
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'What license would you like to use for this project?',
+        choices: ['Apache License 2.0', 'GNU General Public License v3.0', 'MIT License', 'BSD 2-Clause "Simplified" License', 'BSD 3-Clause "New" or "Revised" License', 'Boost Software License 1.0', 'Creative Commons Zero v1.0 Universal', 'Eclipse Public License 2.0', 'GNU Affero General Public License v3.0', 'GNU General Public License v2.0', 'GNU Lesser General Public License v2.1', 'Mozilla Public License 2.0', 'The Unlicense'],
+        // default: 'MIT License',
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'What are your project contribution guidelines?',
+        default: 'Project Contributions',
+        validate: (answer) => (answer.length > 1) ? true : console.log('Please enter a response for your project contributions')
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'What are your project test instructions?',
+        default: 'Project Test Instructions',
+        validate: (answer) => (answer.length > 1) ? true : console.log('Please enter a response for your project test instructions')
+    }
+]
 
 // TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) => err ? console.log(err) : console.log('Your ReadMe has successfully been created!'));
 
-        .then((answers) => {
-            const readMeContent = generateReadMe(answers);
-            // const readMeContent = JSON.stringify(answers, null, '\t')
-
-            // function writeToFile(fileName, data) {
-            fs.writeFile('ReadMe.md', readMeContent, (err) => err ? console.log(err) : console.log('Your ReadMe has successfully been created!')
-            );
-            // fs.writeFile(fileName, JSON.stringify(data, null, '\t'), (err) =>
-            //   err ? console.log(err) : console.log('Your ReadMe has successfully been created!')
-            // )
-        });
+};
 // TODO: Create a function to initialize app
-// function init() { 
-//  generateReadMe;
-// };
-// // call the questions array
+function init() {
+    // call the questions array
+    inquirer.prompt(questions)
+        .then((answers) => {
+            console.log(answers)
+            writeToFile('README,md', generateMarkdown(answers));
+        });
+};
 
-// // Function call to initialize app
-// init();
+
+// Function call to initialize app
+init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,23 +1,54 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-  //let badge = answer of array question
-  //let licenseBadge = link with inserted badge (https://img.shields.io/badge/License-MIT-green)
-  // append licenseBadge to readMe
+  if (license !== undefined) {
+    return `![license](https://img.shields.io/badge/License-${license}-green)`;
+  }
+  return '';
 }
-
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license !== undefined) {
+    return '\n [License](#license)\n';
+  }
+  return '';
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license !== undefined) {
+  return `## License 
 
+Licensed under the ${data.license} license`;
+} return '';
+}
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
-
+  ${renderLicenseBadge(data.license)}
+  ## Table of Contents
+  - [Description](#description)
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Contribution](#contribution)
+  - [Credits](#credits)
+  
+  ## Description
+  ${data.description}
+  ## Installation
+  ${data.installation}
+  ## Usage
+  ${data.usage}
+  ## Contribution
+  ${data.contributing}
+  ## Test Instructions
+  ${data.tests}
+  ## License
+  ${data.license}
+  
+  ## Contact
 `;
 }
 
@@ -26,3 +57,6 @@ module.exports = generateMarkdown;
 
 
 // https://www.markdownguide.org/cheat-sheet/
+
+// const generateReadMe = ({ title, description, installation, usage, license, contributing, tests }) =>
+//     `# ${title}
